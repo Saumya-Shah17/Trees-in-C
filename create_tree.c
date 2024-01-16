f4049716a4a7cd3ddd4917234897295f54ba92b0
Saumya-Shah17 <shahsaumya1712@gmail.com>
2024-01-16 17:54:57 +0530

    Create a BST

---
+++ create_tree.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node{
+int data;
+struct node *left;
+struct node *right;
+}*root;
+
+struct node *create(int x){
+struct node *nw = (struct node*)malloc(sizeof(struct node));
+nw->data = x;
+nw->left = NULL;
+nw->right = NULL;
+return nw;
+}
+
+struct node * tree(struct node *r,int x){
+if(r==NULL){
+r = create(x);
+}
+else if(r->data>x){
+r->left = tree(r->left,x);
+}
+else{
+r->right = tree(r->right,x);
+}
+return r;
+}
+
+void printtree(struct node *ro,int l){
+int i;
+if(ro!=NULL){
+printtree(ro->left,l+1);
+for(i=0;i<l;i++){
+printf("   ");
+}
+printf("%d\n",ro->data);
+printtree(ro->right,l+1);
+}
+}
+int main(){
+int a;
+root =(struct node *)malloc(sizeof(struct node));
+root = NULL;
+while(1){
+printf("Enter data\n");
+scanf("%d",&a);
+if(a==-1)
+break;
+root = tree(root,a);
+}
+printtree(root,0);
+}
